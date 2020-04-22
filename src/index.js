ee25e13601f4eb171801e7f377c335554489cea1
Burhan <bharmal.burhan@hotmail.com>
2020-04-22 19:26:46 +0530

    remove comment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-// import App from './Training/4_Module/App_training'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App appTitle="Person Manager"/>, document.getElementById('root'));
