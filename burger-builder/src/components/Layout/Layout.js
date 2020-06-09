b84103a65db0eb01ffaabcc136a181a0f92fc7c0
Burhan <bharmal.burhan@hotmail.com>
2020-06-09 20:14:31 +0530

    Create burger
    - Add burger items
    - Remove burger items
    - Show total prices
    - Add order now button with disabled state
    - Add More/Less button to add items with disabled state

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Aux from "../../hoc/Auxilary";
-
+import classes from "./Layout.module.css";
+console.log(classes)
 const Layout = (props) => (
   <Aux>
     <div>Toolbar, Sidedrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
