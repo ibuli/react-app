479c7686c2d981c54152b963fb7f422199ef68d9
jainampatel <jainampatel3971@gmail.com>
2022-10-19 18:02:46 +0530

    Added consistent styling in inputs and buttons

--- Training/src/Training/3_Module/UserInput/UserInput.js
+++ Training/src/Training/3_Module/UserInput/UserInput.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const userInput = (props) => {
-  return <input type="text" onChange={props.changed} value={props.currentName}/>
+  display: flex
+  return (
+    <input
+      type="text"
+      onChange={props.changed}
+      value={props.currentName}
+      style={{ padding: '12px 20px', borderRadius: '15px' }}
+    />
+  )
 }
 
 export default userInput
